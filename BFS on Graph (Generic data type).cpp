67babb060099e5ad5004fe1f543e38e438363293
Ved <53895870+ved-sinha@users.noreply.github.com>
2020-01-27 19:15:10 +0530

    Add files via upload

---
+++ BFS on Graph (Generic data type).cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<map>
+#include<queue>
+#include<list>
+#define ll long long int
+using namespace std;
+
+template <typename T>
+class Graph {
+    
+    map<T,list<T>> adjList;
+    
+public: 
+   
+    Graph(){};
+    
+    void addEdge(T u,T v,bool bidir=true) {
+        adjList[u].push_back(v);
+        if(bidir) 
+            adjList[v].push_back(u);
+    } 
+    
+    void printList() {
+        for(auto row:adjList) {
+            cout<<row.first<<" -> ";
+            for(auto elements:row.second) {
+                cout<<elements<<" , ";
+            }
+            cout<<endl;
+        }
+    }
+    
+    void bfs(T src) {
+        queue<T> Q;
+        map<T,bool> visited;
+        Q.push(src);
+        visited[src]=true;
+        
+        while(!Q.empty()) {
+            T node=Q.front();
+            cout<<node<<" ";
+            Q.pop();
+            
+            for(auto neighour:adjList[node]) {
+                if(visited[neighour]==false) {
+                    Q.push(neighour);
+                    visited[neighour]=true;
+                }
+            }
+        }
+    }
+    
+};
+
+int main() {
+	Graph<int> g;
+	g.addEdge(0,1);
+	g.addEdge(0,4);
+	g.addEdge(1,2);
+	g.addEdge(2,4);
+	g.addEdge(3,4);
+	g.addEdge(2,3);
+	g.addEdge(3,5);
+	g.printList();
+	cout<<endl;
+	g.bfs(0);
+	return 0;
+}
